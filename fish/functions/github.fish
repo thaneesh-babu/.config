443e9c1258fedfa323e31424c39d550964f39d1c
Thaneesh Babu <63804742+thaneesh-babu@users.noreply.github.com>
2022-09-13 18:44:11 -0400

    update github fish function

--- fish/functions/github.fish
+++ fish/functions/github.fish
@@ -1,3 +1,3 @@
-function github --wraps='cd ~/Documents/GitHub/ && code .' --description 'alias github=cd ~/Documents/GitHub/ && code .'
+function github --wraps='cd ~/Documents/GitHub/ && code $argv[1]' --description 'alias github=cd ~/Documents/GitHub/ && code $argv[1]'
   cd ~/Documents/GitHub/ && code $argv[1]; 
 end
