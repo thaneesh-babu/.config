b07da2e1c7924cb0717b2259101d9c0758574d7c
Thaneesh Babu <gkthaneesh2@gmail.com>
2023-02-11 05:03:14 -0500

    update polybar and i3

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-# Add this script to your wm startup file.
-
-DIR="$HOME/.config/polybar"
-
 # Terminate already running bar instances
-killall -q polybar
+# If all your bars have ipc enabled, you can use
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+# Launch bar
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar -q primary 2>&1 | tee -a /tmp/polybar1.log & disown
