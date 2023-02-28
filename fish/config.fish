65ccefdecd63c87ecd32d8ed6cadc61e55dd40a0
Thaneesh Babu <gkthaneesh2@gmail.com>
2023-02-28 00:40:00 -0500

    update fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -4,10 +4,12 @@ if status is-interactive
 end
 starship init fish | source
 export GEM_HOME="$HOME/.gem"
+export ANDROID_HOME="$HOME/Android/Sdk"
+# export PATH=$PATH:"$ANDROID_HOME/emulator"
+# export PATH=$PATH:"$ANDROID_HOME/platform-tools"
 test -s /home/thans/.nvm/nvm.fish; and source /home/thans/.nvm/nvm.fish
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 eval /usr/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
-
